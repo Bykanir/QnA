f7fea80dbc61d10048a1b255a9e043bdbb6f4e29
bykanir <bykanir@gmail.com>
2021-12-04 00:07:50 +0300

    Rspec for answers#create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,5 +3,26 @@ class AnswersController < ApplicationController
   def index; end
 
   def new; end
-  
+
+  def create
+    @answer = question.answers.create(answer_params)
+
+    if @answer.save
+      redirect_to question
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question
+    @question ||= Question.find(params[:question_id])
+  end
+
+  helper_method :question
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 end
