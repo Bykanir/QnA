4a328fa93d6fb37e16ff939ea68942d311a426c8
bykanir <bykanir@gmail.com>
2021-12-13 23:34:18 +0300

    added spec for sign up

---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+feature 'User can sign up', %q{
+  In order to ask questions
+  As an unregistered user
+  I'd like to be able to sign up
+} do
+
+  given(:user) { create(:user) }
+
+  background { visit new_user_registration_path }
+
+  scenario 'Unregistered user tries to sign up' do
+    fill_in 'Email', with: 'new_user@test.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  end
+
+  scenario 'Sign up error, email is invalid.' do
+    fill_in 'Email', with: 'new_usertest.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '12345678'
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Email is invalid'
+  end
+
+  scenario 'Sign up error, email already registered.' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password_confirmation
+    click_on 'Sign up'
+
+    expect(page).to have_content 'Email has already been taken'
+  end
+
+  scenario 'Sign up error, fields are empty .' do
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: ''
+    fill_in 'Password confirmation', with: ''
+    click_on 'Sign up'
+
+    expect(page).to have_content "Email can't be blank"
+    expect(page).to have_content "Password can't be blank"
+  end
+
+  scenario 'Sign up error, fields are empty .' do
+    fill_in 'Email', with: 'new_user@test.com'
+    fill_in 'Password', with: '12345678'
+    fill_in 'Password confirmation', with: '18273645'
+    click_on 'Sign up'
+
+    expect(page).to have_content "Password confirmation doesn't match Password"
+  end
+  
+end
+
+
