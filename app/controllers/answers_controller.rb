8079ce5ab3a66c6877d5f1e46262256d08e7605c
bykanir <bykanir@gmail.com>
2021-12-27 22:21:55 +0300

    added spec for answer, only authenticated user can create answer

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,5 +1,7 @@
 class AnswersController < ApplicationController
 
+  before_action :authenticate_user!, only: [:create]
+
   def index; end
 
   def new; end
--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -1,26 +1,41 @@
 require 'rails_helper'
 
-feature 'User can create answer' do
+feature 'Authenticated user can create answer' do
 
   given(:user) { create(:user) }
   given!(:question) { create(:question) }
-  
-  scenario 'Answer the question' do
-    visit question_path(question)
 
+  describe "Authenticated user" do
+    background do
+      sign_in(user)
+  
+      visit question_path(question)
+    end
+
+    scenario 'answer the question' do
+      fill_in 'Body', with: 'My new answer'
+      click_on 'Send'
+  
+      expect(page).to have_content 'My new answer'
+    end
+
+    # scenario 'answer the question with errors' do
+    #   visit question_path(question)
+    #   save_and_open_page
+
+    #   click_on 'Send'
+
+    #   expect(page).to have_content "Body can't be blank"
+    # end
+  end
+  
+  scenario 'Unauthenticated user tries to answer a question' do
+    visit question_path(question)
+    
     fill_in 'Body', with: 'My new answer'
     click_on 'Send'
 
-    expect(page).to have_content 'My new answer'
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
   end
-
-  # scenario 'Form is empty' do
-  #   visit question_path(question)
-  #   save_and_open_page
-
-  #   click_on 'Send'
-
-  #   expect(page).to have_content "Body can't be blank"
-  # end
 
 end
