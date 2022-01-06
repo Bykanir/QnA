e7776be06c0ac27e7c0921c4286e7eb5a0724d21
bykanir <bykanir@gmail.com>
2022-01-07 00:50:35 +0300

    fix bugs for spec question#index

--- spec/features/question/index_spec.rb
+++ spec/features/question/index_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 feature 'User can see all questions' do
 
   given(:user) { create(:user) }
-  given!(:question) { create(:question, author: user) }
+  given!(:questions) { create_list(:question, 2, author: user) }
   
   scenario 'User see all question' do
     visit questions_path
 
-    expect(page).to have_content 'MyString'
+    questions.each { |question| expect(page).to have_content "#{question.title}" }
   end
 
 end
