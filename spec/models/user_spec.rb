af729ec6c466956e38bb76ca455972721066f1a4
bykanir <bykanir@gmail.com>
2022-01-08 21:03:47 +0300

    add spec for method author_of? model user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,22 @@ RSpec.describe User, type: :model do
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe 'author_of?' do
+    let(:author) { create(:user) }
+    let(:user) { create(:user) }
+
+    it 'user author' do
+      question = create(:question, author: author)
+      
+      expect(author).to be_author_of(question)
+    end
+    
+    it 'user not author' do
+      question = create(:question, author: author)
+      
+      expect(user).to_not be_author_of(question)
+    end
+  end
+  
 end
