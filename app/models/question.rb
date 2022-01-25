097ae8717e1c01b1a88d9f50bce25196e28e1ebf
bykanir <bykanir@gmail.com>
2022-01-25 23:36:57 +0300

    add multiple files

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,9 +2,9 @@ class Question < ApplicationRecord
 
   has_many :answers, dependent: :destroy
 
-  has_one_attached :file
-
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+
+  has_many_attached :files
 
   validates :title, :body, presence: true
 
