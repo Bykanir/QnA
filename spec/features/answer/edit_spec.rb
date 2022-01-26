fb5bfcf795288893cf6991a3192bbe3e95953ede
bykanir <bykanir@gmail.com>
2022-01-26 23:56:00 +0300

    add multiple files for answer#edit

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -44,6 +44,18 @@ feature 'User can edit answer', %q{
         expect(page).to have_content "Body can't be blank"
        end
     end
+
+    scenario 'edits answer with attached file' do
+      within '.answers' do
+        fill_in 'Your answer',	with: 'Edited answer'
+
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+  
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+       end
+    end
   end
 
   scenario "tries to edit other user's question", js: true do
